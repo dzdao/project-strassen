1436f3e64af8d5f5ae9bfdec5125eefa6b93cf90
dzdao <davidzdao@gmail.com>
2017-09-27 22:15:19 -0700

    Multiply NxN matrices with a sequential and naive approach

--- src/main/scala/Strassen.scala
+++ src/main/scala/Strassen.scala
@@ -3,6 +3,24 @@ import akka.actor._
 
 object Strassen {
 
+  def naiveMultiply(a: Array[Array[Int]], b: Array[Array[Int]]): Array[Array[Int]] = {
+    val result = Array.ofDim[Int](a.length, b.length)
+
+    for(i <- 0 until a.length) {
+      for(j <- 0 until a.length) {
+        var element = 0
+
+        for(k <- 0 until a.length) {
+          var product = a(i)(k) * b(k)(j)
+          element += product
+        }
+
+        result(i)(j) = element
+      }
+    }
+    result
+  }
+
   def generateMatrix(dim: Int): Array[Array[Int]] = {
     val rand = Random
     val matrix = Array.ofDim[Int](dim, dim)
@@ -12,7 +30,6 @@ object Strassen {
         matrix(i)(j) = rand.nextInt(100)
       }
     }
-
     matrix
   }
 
@@ -24,12 +41,19 @@ object Strassen {
   }
 
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
-    val dim = 100
 
-    val randMatrix = generateMatrix(dim)
-    printMatrix(randMatrix)
+    val dim = 2
 
+    val a = generateMatrix(dim)
+    printMatrix(a)
+    println()
+
+    val b = generateMatrix(dim)
+    printMatrix(b)
+    println()
+
+    val c = naiveMultiply(a, b)
+    printMatrix(c)
   }
 }
 
